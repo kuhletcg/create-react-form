af859d1217b622da0ecfacab7814b611935a415a
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-02 12:07:15 +0000

    add files

---
+++ src/form.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function Form(params) {
+    const [name, setName] = useState('')
+    const handleChange=(event)=> {
+        setName(event.target.value );
+    }   
+    const handleSubmit=()=>{
+        alert('A name was submitted: ' + name);
+     
+    }
+     return(
+        <div>
+            Name:
+            <input onChange={handleChange} type="text" value={name} />
+            <input onClick={handleSubmit} type="submit" value="Submit" />
+            </div>
+    )
+            
+    
+    
+}
