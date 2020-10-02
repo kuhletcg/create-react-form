bc57d9cec02df3b1a6db6f04ded527dd48aece7e
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-02 12:54:43 +0000

    add map information

--- src/form.js
+++ src/form.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 
 export default function Form(params) {
-    const [name, setName] = useState('')
+    const [information, setInformation] = useState({name: '',surname: ''})
+    const [data, setData] = useState([])
     const handleChange=(event)=> {
-        setName(event.target.value );
+        setInformation({...information,[event.target.name]: event.target.value});
     }   
     const handleSubmit=()=>{
-        alert('A name was submitted: ' + name);
+       setData(information) 
+        console.log(information) 
     }
      return(
         <div>
             Name:
-            <input onChange={handleChange} type="text" value={name} />
+            <input onChange={handleChange} name='name' type="text" value={information.name} />
             Surname:
-            <input onChange={handleChange} type="text" value={name} />
-            <input onClick={handleSubmit} type="submit" value="Submit" />
+            <input onChange={handleChange} name='surname' type="text" value={information.surname} />
+            <input onClick={handleSubmit} type="submit" value="Submit" /><br/>
+            {/* {data.map(t => <div>{t.name}  {t.surname}</div>)} */}
+            Name: {data.name} <br/>
+            Surname:{data.surname}
             </div>
     )
             
