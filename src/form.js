79a51fe89d68d0cb0cb04c41e71581a5ebdccde1
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-02 12:20:13 +0000

    add input surname

--- src/form.js
+++ src/form.js
@@ -7,11 +7,12 @@ export default function Form(params) {
     }   
     const handleSubmit=()=>{
         alert('A name was submitted: ' + name);
-     
     }
      return(
         <div>
             Name:
+            <input onChange={handleChange} type="text" value={name} />
+            Surname:
             <input onChange={handleChange} type="text" value={name} />
             <input onClick={handleSubmit} type="submit" value="Submit" />
             </div>
